70293089e4fe8ae469c1be9e8675413706b93327
SHADOW12021 <gsoccer2003@gmail.com>
2025-01-18 14:59:50 -0500

    CONTROLZ

--- src/App.tsx
+++ src/App.tsx
@@ -66,7 +66,7 @@ function App() {
       });
     };
 
-    // Check if geolocation is supported
+    // supported?
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(handleSuccess, handleError);
     } else {
